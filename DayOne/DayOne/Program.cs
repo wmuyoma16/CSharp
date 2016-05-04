7a3d07084f14da718fd0735bfee52dd24087bc5e
wilo <w.muyoma@gmail.com>
2016-05-04 14:41:56 -0700

    Functioning day one challenge

---
+++ DayOne/DayOne/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DayOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myNum1 = "";
+            Console.WriteLine("Please inter number press return: ");
+            myNum1 = Console.ReadLine();
+            if (myNum1 == "4 8 15 16 23 42")
+                Console.WriteLine("Recalibrating energy core... Complete.");
+            else
+                Console.WriteLine("FAILURE");
+        }
+    }
+}
+
+
